7a280a44dff1cffcf513e4419dd183c3c4072433
DouglasEde <briancarter340@gmail.com>
2023-02-04 11:17:09 -0800

    added Header section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
+import Header from './Header.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-         Code edited
-        </p>
-
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <> 
+    <header>Emilia's Portfolio Est. 2021</header>
+    </>
+   
+  )
+}
+
+export default Header
