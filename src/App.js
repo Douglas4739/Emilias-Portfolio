bfb983685582543e08a0fac7a7bb2886e2debbe6
DouglasEde <briancarter340@gmail.com>
2023-02-04 11:22:32 -0800

    added Value component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Header.js';
+import Value from './Value.js';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Value />
     </div>
   );
 }
---
+++ src/Value.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Value.css';
+
+function Value() {
+  return (
+    <div className="value">Value</div>
+  )
+}
+
+export default Value
