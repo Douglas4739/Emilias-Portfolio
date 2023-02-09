f4d9ebeb5c2210c0fb26ae83fd6d8decea79715e
DouglasEde <briancarter340@gmail.com>
2023-02-08 16:29:15 -0800

    added some content/style to Contributions

--- src/Contributions.js
+++ src/Contributions.js
@@ -3,7 +3,18 @@ import './Contributions.css';
 
 function Contributions() {
   return (
-    <div className="contributions">Contributions</div>
+    <>
+      <div className="contributions">
+        <ul className="contributors">
+          <li className="contributor">Contributor #1</li>
+          <li className="contributor">Contributor #2</li>
+          <li className="contributor">Contributor #3</li>
+          <li className="contributor">Contributor #4</li>
+          <li className="contributor">Contributor #5</li>
+        </ul>
+      </div>
+    </>
+
   )
 }
 
