fb301e261a68abc6369e27ccad68a0becb0e6075
DouglasEde <briancarter340@gmail.com>
2023-02-28 19:44:23 -0800

    cool error bro

--- src/Graph.js
+++ src/Graph.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js';
 import './Graph.css';
 
-function Graph() {
-  return (
-    <>
-      <div className="graphContainer">
-        <div className="graph">Graph</div>
-      </div>
-    </>
+// function Graph() {
+//   return (
+//     <>
+//       <div className="graphContainer">
+//         <div className="graph">Graph</div>
+//       </div>
+//     </>
 
     
-  )
+//   )
+// }
+
+// export default Graph;
+
+
+function Graph() {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const chartCanvas = chartRef.current.getContext('2d');
+
+    new Chart(chartCanvas, {
+      type: 'line', // or 'bar', 'pie', etc.
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'Sales',
+            data: [12, 19, 3, 5, 2, 3, 15],
+            borderColor: 'green',
+            fill: false,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    });
+  }, []);
+
+  return (
+    <div className="graphContainer">
+      <canvas ref={chartRef} className="graph"></canvas>
+    </div>
+  );
 }
 
-export default Graph
+export default Graph;
