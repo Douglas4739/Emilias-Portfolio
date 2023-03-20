aa0f80e216593fd56c339b79023d0359599079b3
DouglasEde <briancarter340@gmail.com>
2023-03-19 21:05:52 -0700

    minor adjustments to text

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ function About() {
     <p className="about">
       This labor of love was conceived months after learning of Emilia's pending arrival into this world. And with it, the goal to give my progeny access to tools that were'nt as readily available to me.
       I committed to adding $25 a week to a stock brokerage account and build a portfolio geared towards long term growth and dividend income.
-      Some friends and family have been gracious enough to donate to Emilia's future. And I'll admit that I've made some extraciricular contributions here and there.
+      Some family have been gracious enough to donate to Emilia's future. And I'll admit that I've made some extraciricular contributions here and there.
       This is already so much more than what I thought it would be at this point, but my heart is full of appreciation for the donors and excitement for the future of this project.
     </p>
   )
