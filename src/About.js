50c3949d687f83ff0a81507460ab55243d69070f
DouglasEde <briancarter340@gmail.com>
2023-03-19 21:06:43 -0700

    more minor adjustments

--- src/About.js
+++ src/About.js
@@ -3,12 +3,16 @@ import './About.css';
 
 function About() {
   return (
+    <>    
     <p className="about">
       This labor of love was conceived months after learning of Emilia's pending arrival into this world. And with it, the goal to give my progeny access to tools that were'nt as readily available to me.
       I committed to adding $25 a week to a stock brokerage account and build a portfolio geared towards long term growth and dividend income.
       Some family have been gracious enough to donate to Emilia's future. And I'll admit that I've made some extraciricular contributions here and there.
       This is already so much more than what I thought it would be at this point, but my heart is full of appreciation for the donors and excitement for the future of this project.
     </p>
+    <p>Be sure to check this out on screens larger than 650px to see progress chart :)</p>
+    </>
+
   )
 }
 
