30d3752b713e7e0956f498b2ad1b42a3c17ce35d
DouglasEde <briancarter340@gmail.com>
2023-03-19 20:15:50 -0700

    added new contributor list

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import Header from './Header.js';
 import Value from './Value.js';
 import Graph from './Graph.js';
 import Contributions from './Contributions.js';
+import ContributorsList from './ContributorsList.js';
 import About from './About.js';
 import Footer from './Footer.js';
 
 
 function App() {
+  const contributors = [
+    { name: 'John Doe', amount: 100 },
+    { name: 'Jane Smith', amount: 50 },
+    { name: 'Bob Johnson', amount: 25 },
+  ];
+
   return (
     <div className="App">
       <Header />
       <Value />
       <Graph />
       <Contributions />
+      <ContributorsList contributors={contributors} />
       <About />
       <Footer />
     </div>
